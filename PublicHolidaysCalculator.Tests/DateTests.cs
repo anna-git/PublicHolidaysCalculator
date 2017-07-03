e6931028469518b71fdf344b8a5211dfe1a8b96c
Anna Yafi <annayafi@gmail.com>
2017-07-03 12:36:13 +0200

    operations

--- PublicHolidaysCalculator.Tests/DateTests.cs
+++ PublicHolidaysCalculator.Tests/DateTests.cs
@@ -8,10 +8,10 @@
         [Fact]
         public void CheckEquality()
         {
-            var date =new Date(2011, 12, 1);
-            var date1 =new Date(2011, 12, 1);
+            var date = new Date(2011, 12, 1);
+            var date1 = new Date(2011, 12, 1);
             Assert.Equal(date1, date);
-            Assert.True(date ==  date1);
+            Assert.True(date == date1);
         }
 
         [Fact]
@@ -24,6 +24,22 @@
         }
 
         [Fact]
-        public void CheckMonth() => Assert.Throws<ArgumentOutOfRangeException>(()=>new Date(2011, 13, 2));
+        public void CheckGreaterThan()
+        {
+            var date = new Date(2012, 1, 1);
+            var date1 = new Date(2011, 12, 1);
+            Assert.True(date > date1);
+            Assert.True(date1 < date);
+            Assert.False(date == date1);
+        }
+        [Fact]
+        public void CheckSubstract()
+        {
+            var date = new Date(2011, 12, 1);
+            var date1 = new Date(2011, 12, 2);
+            Assert.Equal(TimeSpan.FromDays(1), date1 - date);
+        }
+        [Fact]
+        public void CheckMonth() => Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2011, 13, 2));
     }
 }
--- PublicHolidaysCalculator/Date.cs
+++ PublicHolidaysCalculator/Date.cs
@@ -8,26 +8,30 @@ namespace PublicHolidaysCalculator
     {
         public Date(int year, int month, int day)
         {
-            if(month > 12)
+            if (month > 12)
                 throw new ArgumentOutOfRangeException(nameof(month));
             Month = month;
             Day = day;
             Year = year;
+            DateTime = new DateTime(Year, Month, Day);
         }
-        public int Year { get; set; }
-        public int Month { get; set; }
-        public int Day { get; set; }
-        public DateTime ToDateTime() => new DateTime(Year, Month, Day);
-        public string ToString(string format) => ToDateTime().ToString(format);
-        public override string ToString() => ToDateTime().ToString(CultureInfo.InvariantCulture);
+        public int Year { get; }
+        public int Month { get; }
+        public int Day { get;  }
+        public DateTime DateTime { get; }
+        public string ToString(string format) => DateTime.ToString(format);
+        public override string ToString() => DateTime.ToString(CultureInfo.InvariantCulture);
         public static bool operator ==(Date dt, Date other) => IsEqualTo(dt, other);
         public static bool operator !=(Date dt, Date other) => dt.Year == other.Year || dt.Month != other.Month || dt.Day != other.Day;
+        public static bool operator <(Date dt, Date other) => dt.DateTime < other.DateTime;
+        public static bool operator >(Date dt, Date other) => dt.DateTime > other.DateTime;
+        public static TimeSpan operator -(Date dt, Date other) => dt.DateTime - other.DateTime;
         private static bool IsEqualTo(Date dt, Date other) => dt.Day == other.Day && dt.Month == other.Month && dt.Year == other.Year;
         public override bool Equals(object obj)
         {
-            if(!(obj is Date)) throw new ArgumentException("Obj is not Date type");
-            return IsEqualTo(this, (Date) obj);
+            if (!(obj is Date)) throw new ArgumentException("Obj is not Date type");
+            return IsEqualTo(this, (Date)obj);
         }
-        public override int GetHashCode() => ToDateTime().GetHashCode();
+        public override int GetHashCode() => DateTime.GetHashCode();
     }
 }
